49476cceef013798864ebb3fdbc3783348b868a1
Benoit Bovy <benbovy@gmail.com>
2019-01-28 14:14:39 +0100

    include flexure in Python bindings

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     license="GPLv3",
     packages=['fastscapelib_fortran'],
     cmake_args=['-DBUILD_PYTHON_MODULE=ON',
-                '-DBUILD_FASTSCAPELIB_STATIC=OFF'],
+                '-DBUILD_FASTSCAPELIB_STATIC=OFF',
+                '-DUSE_FLEXURE=ON'],
     cmake_languages=('C', 'Fortran'),
     cmake_minimum_required_version='3.5'
 )
