59bdb1be86882ca44afbe9b211548c2302518594
Benoit Bovy <benbovy@gmail.com>
2019-10-13 14:08:18 +0200

    release v2.8.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="fastscapelib-fortran",
-    version="2.8.0dev",
+    version="2.8.1",
     description=("A library of efficient algorithms"
                  "for landscape evolution modeling"),
     author='Jean Braun',
