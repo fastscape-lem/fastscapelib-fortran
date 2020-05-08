18a62c3dd30732ff137fb3ca75b3b8e800dc2ea8
Benoit Bovy <benbovy@gmail.com>
2020-05-08 14:16:07 +0200

    update to scikit-build 0.11
    Remove cmake modules imported from scikit-build (doesn't make sense to
    maintain it here).
    
    Remove BUILD_PYTHON_MODULE option (only support building the Python
    bindings via scikit-build)

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ setup(
     author='Jean Braun',
     license="GPLv3",
     packages=['fastscapelib_fortran'],
-    cmake_args=['-DBUILD_PYTHON_MODULE=ON',
-                '-DBUILD_FASTSCAPELIB_STATIC=OFF',
+    cmake_args=['-DBUILD_FASTSCAPELIB_STATIC=OFF',
                 '-DUSE_FLEXURE=ON'],
     cmake_languages=('C', 'Fortran'),
     cmake_minimum_required_version='3.5'
